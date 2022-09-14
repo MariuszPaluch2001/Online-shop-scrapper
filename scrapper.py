6cba9aa1cf52bddbbae9f708568093c61848aa8d
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-09-14 18:10:22 +0200

    Add separate script to scrap data with arguments parser.

--- scrapper.py
+++ scrapper.py
@@ -5,10 +5,28 @@ from src.web_crawler.scrap import scrap
 
 import argparse
 
+"""
+
+    Script to run without any GUI.
+    If you want more info about args type into terminal:
+
+    python scrapper.py --help
+
+"""
             
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--number_pages", type=int, default = 1,
+                    help="Number of page to scrap")
+    parser.add_argument("-q", "--query", type=str,
+                    help="Name of product to scrap")
+    parser.add_argument("--do-print", default=False, action="store_true", 
+                    help = "Print product info to terminal.")
+    parser.add_argument("--no-write-db", default=False, action="store_true", 
+                    help = "Do not write data to database.")
+    args = parser.parse_args()
     url = Cen_URL()
     c_scrapper = Cen_Scrapper()
     db = MongoDB("scrapper")
     db_support = MongoDB_Support(db)
-    scrap("Mongo", 3, url, c_scrapper, db, db_support, True, False)
+    scrap(args.query, args.number_pages, url, c_scrapper, db, db_support, args.do_print, args.no_write_db)
--- src/web_crawler/scrap.py
+++ src/web_crawler/scrap.py
@@ -1,6 +1,6 @@
 from src.web_crawler.get_page import get_page_html
 
-def scrap(query, numb_pages, url, scrapper, db, db_support, print_flag, add_to_db_flag):
+def scrap(query, numb_pages, url, scrapper, db, db_support, print_flag = False, no_add_to_db = False):
     for page in url.multi_page_query(query, numb_pages):
         soup = get_page_html(page)
         result_gen = scrapper.scrap_product_info(soup)
@@ -9,5 +9,5 @@ def scrap(query, numb_pages, url, scrapper, db, db_support, print_flag, add_to_d
             for product in products:
                 product.print()
         
-        if add_to_db_flag:
+        if not no_add_to_db:
             db_support.insert("data", [res.get_json() for res in products])
