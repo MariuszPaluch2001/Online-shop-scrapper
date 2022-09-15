80747d509436db01ca7d55c6606e3a6a7dec69e2
MariuszPaluch2001 <mariuszpaluch001@gmail.com>
2022-09-15 17:29:41 +0200

    Working argparse in query script.

--- query.py
+++ query.py
@@ -16,13 +16,17 @@ import argparse
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-p-min", "--price-min", type=int, default = None,
-                    help="Number of page to scrap")
+                    help="Minimal value in price bound")
     parser.add_argument("-p-max", "--price-max", type=int, default = None,
-                    help="Number of page to scrap")
+                    help="Maximal value in price bound")
+    parser.add_argument("-d-min", "--date-min", type=str, default = None,
+                    help=R"Begining date in date range. Date format: 'd/m/y H:M:S'")
+    parser.add_argument("-d-max", "--date-max", type=str, default = None,
+                    help=R"Ending date in date range. Date format: 'd/m/y H:M:S'")
     parser.add_argument("-q", "--query", type=str,
-                    help="Name of product to scrap")
+                    help="Name of product to query")
     parser.add_argument("-c", "--currency", type=str, default = "PLN",
-                    help="Name of product to scrap")
+                    help="Currency of product in query")
     parser.add_argument("--do-print", default=False, action="store_true", 
                     help = "Print product info to terminal.")
     
@@ -32,7 +36,10 @@ if __name__ == "__main__":
     db = MongoDB("scrapper")
     db_support = MongoDB_Support(db)
     mongo_q = MongoDB_Queries(db_support)
-    date1 = datetime.strptime('6/9/22 12:00:00', '%d/%m/%y %H:%M:%S')
-    date2 = datetime.strptime('15/9/22 16:00:00', '%d/%m/%y %H:%M:%S')
-    print((args.price_min,args.price_max))
-    query_search(mongo_q, "data", (args.price_min,args.price_max), (date1, date2), args.query, args.currency, args.do_print)
+    
+    date_min = datetime.strptime(args.date_min, '%d/%m/%y %H:%M:%S')
+    date_max = datetime.strptime(args.date_max, '%d/%m/%y %H:%M:%S')
+
+    query_search(mongo_q, "data", (args.price_min,args.price_max), (date_min, date_max), 
+                args.query, args.currency, args.do_print
+    )
